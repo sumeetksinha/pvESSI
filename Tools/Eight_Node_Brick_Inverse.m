8d9407bd5e4e7798066e526b796141375651d62b
Sumeet Kumar Sinha <sumeet.kumar507@gmail.com>
2016-08-13 15:35:32 -0700

    Adding Gauss stress interpolation to nodes for 20 and 27 node brick elements

--- Tools/Eight_Node_Brick_Inverse.m
+++ Tools/Eight_Node_Brick_Inverse.m
@@ -1,18 +1,34 @@
 function Eight_Node_Brick_Inverse = Eight_Node_Brick_Inverse()
 
-	Eight_Brick_Coordinates = Build_Brick_Coordinates();
-	Eight_Brick_Coordinates = Eight_Brick_Coordinates(1:8,:);
+	% shape function coordinates  coefficients
+	Brick_Coordinates(1 ,:)=[ 1, 1, 1]; 
+	Brick_Coordinates(2 ,:)=[-1, 1, 1]; 
+	Brick_Coordinates(3 ,:)=[-1,-1, 1]; 
+	Brick_Coordinates(4 ,:)=[ 1,-1, 1]; 
+	Brick_Coordinates(5 ,:)=[ 1, 1,-1]; 
+	Brick_Coordinates(6 ,:)=[-1, 1,-1]; 
+	Brick_Coordinates(7 ,:)=[-1,-1,-1]; 
+	Brick_Coordinates(8 ,:)=[ 1,-1,-1]; 
+
+	% gauss coordinates coefficients 
+	Brick_8_Gauss_Coordinates(1,:)=[-1,-1,-1];
+	Brick_8_Gauss_Coordinates(2,:)=[-1,-1, 1];
+	Brick_8_Gauss_Coordinates(3,:)=[-1, 1,-1];
+	Brick_8_Gauss_Coordinates(4,:)=[-1, 1, 1];
+	Brick_8_Gauss_Coordinates(5,:)=[ 1,-1,-1];
+	Brick_8_Gauss_Coordinates(6,:)=[ 1,-1, 1];
+	Brick_8_Gauss_Coordinates(7,:)=[ 1, 1,-1];
+	Brick_8_Gauss_Coordinates(8,:)=[ 1, 1, 1];
 
 	Eight_Node_Brick_Inverse = zeros(8,8);
 
-	SQRT_3 = sqrt(1/3);
+	SQRT_3 = sqrt(1.0/3.0);
 
-
-	for j=1:8
-		for i=1:8
-			Eight_Node_Brick_Inverse(j,i) = 1/8*(1+Eight_Brick_Coordinates(j,1)*Eight_Brick_Coordinates(i,1)*SQRT_3)* ...
-												(1+Eight_Brick_Coordinates(j,2)*Eight_Brick_Coordinates(i,2)*SQRT_3)* ...
-												(1+Eight_Brick_Coordinates(j,3)*Eight_Brick_Coordinates(i,3)*SQRT_3);
+	for j=1:8	% Gauss Point Coordinates
+		for i=1:8 % Shape Functions 
+			Eight_Node_Brick_Inverse(j,i) = 1/8*(1+Brick_Coordinates(i,1)*Brick_8_Gauss_Coordinates(j,1)*SQRT_3)* ...
+												(1+Brick_Coordinates(i,2)*Brick_8_Gauss_Coordinates(j,2)*SQRT_3)* ...
+												(1+Brick_Coordinates(i,3)*Brick_8_Gauss_Coordinates(j,3)*SQRT_3);
 		end
 	end
 
